b8186f71b69d1aa08a8210e8f10c01e616eb5982
Michiko  Inoue <inouem888@gmail.com>
2024-10-29 14:03:11 +0000

    Add excerpt in the post model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = (
-    (0, "Draft"),
-    (1, "Published")
-)
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
@@ -16,3 +13,4 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
